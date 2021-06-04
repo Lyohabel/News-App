c3c1d7ec9685c81323072d5eb3720032ff283b2d
Lev <lyoha.bel2020@gmail.com>
2021-06-04 21:08:32 +0300

    got API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Header from "./Components/Header/Header"
+
 import './App.css';
 
 function App() {
+  const[dataStatus, setDataStatus] = useState(false)
+  const[news, setNews] = useState([])
+ 
+
+  const getData = () => {
+    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=c5c59399c298440c8978f43a60953157')
+    .then((res) => {
+      return res.text()
+    })
+    .then((data) => {
+      localStorage.setItem('data', data)
+      const resp = JSON.parse(data)
+      setNews(resp.articles)
+      setDataStatus(true)
+      console.log(news[0])    
+    })           
+  } 
+
+  useEffect(() => {
+    if (dataStatus === false) getData()   
+  }, [dataStatus, news])
+
   return (
-    <div>
-     123456789
-    </div>
+    <Header/>
   );
 }
 
