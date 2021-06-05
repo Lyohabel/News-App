05109cc066c03bc5d607c55585a5e279623ebb0b
Lev <lyoha.bel2020@gmail.com>
2021-06-05 18:02:49 +0300

    Articles created

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import React, {useState, useEffect} from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Header from "./Components/Header/Header"
+import Articles from "./Components/Articles/Articles"
 
 import './App.css';
 
 function App() {
-  const[dataStatus, setDataStatus] = useState(false)
+  const[dataStatus, setDataStatus] = useState(false)  
   const[news, setNews] = useState([])
  
 
   const getData = () => {
-    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=c5c59399c298440c8978f43a60953157')
+    fetch('https://newsapi.org/v2/top-headlines?sources=cnn,bbc-news,associated-press,bloomberg,the-wall-street-journal&apiKey=c5c59399c298440c8978f43a60953157')
     .then((res) => {
       return res.text()
     })
-    .then((data) => {
-      localStorage.setItem('data', data)
-      const resp = JSON.parse(data)
-      setNews(resp.articles)
-      setDataStatus(true)
-      console.log(news[0])    
-    })           
-  } 
+    .then((data) => {      
+      setNews(JSON.parse(data).articles)
+      //setDataStatus(true)
+    })    
+  }
+
+//   const getData = async () => {    
+//     const response = await fetch('https://newsapi.org/v2/top-headlines?sources=cnn,bbc-news,associated-press,bloomberg,the-wall-street-journal&apiKey=c5c59399c298440c8978f43a60953157')
+//     const resp = await response.json()
+//     const articles = await resp.articles   
+//     setNews(articles)    
+// }
 
   useEffect(() => {
     if (dataStatus === false) getData()   
-  }, [dataStatus, news])
+  }, [dataStatus])
 
+  console.log(news)
+  
   return (
-    <Header/>
+    <BrowserRouter >
+      <Header/>
+
+      <Switch>
+        <Route exact path='/'>
+          <Articles/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Articles/Articles.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as styles from './Articles.module.css'
+
+function Articles() {
+
+    return (    
+        <section className={styles.articles}>
+            <div className="container">
+                <div className={styles.list}>
+                    <h2>XXXXX</h2>            
+                </div>                
+            </div>            
+        </section>    
+      )
+}
+
+export default Articles;
